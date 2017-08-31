90cdccb229ab55087a3a2cb874c88ebe949ce96c
Timothy McCord <mccordt@vir.hh.nku.edu>
2017-08-31 14:58:23 -0400

    Uploading starter code.

---
+++ generate_commands.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+# CIT 483/583-001 Fall 2017
+
+# Starter code given with the assignment
+
+users = {'smithj1' => 'John Smith', 'doej1' => "Jane Doe", 'publicj1' => "John Q. Public", 'mccordt' => "Tim McCord" }
+words = ['Care' , 'less', 'Toys', 'game', 'rand', 'Ruby', 'star', 'Lite']
+seps = ['−', '%', '&', '@', '#', '!' ]
+
+users.each_key do |user|
+
+  ##### THE BULK OF YOUR CODE GOES HERE #####
+
+end
+
+
